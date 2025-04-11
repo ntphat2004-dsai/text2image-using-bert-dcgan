dc7d22b46888ff4fd2256aed8407838bf1ef389e
unknown <nguyenthphat0106@gmail.com>
2025-04-10 19:13:50 -0700

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ FAKE_LABEL = 0.1
 SAVE_MODEL_PATH = "models"
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 DATASET_URL = "https://drive.google.com/uc?id=1JJjMiNieTz7xYs6UeVqd02M3DW4fnEfU"
-CAPTIONS_PATH = ".\cvpr2016_flowers\captions"
-IMAGES_PATH = ".\cvpr2016_flowers\images"
+CAPTIONS_PATH = "./cvpr2016_flowers/content/cvpr2016_flowers/captions"
+IMAGES_PATH = "./cvpr2016_flowers/content/cvpr2016_flowers/images"
